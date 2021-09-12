18fedb151a01d13d03865a7861da371aab10a09f
RudinP <65944851+RudinP@users.noreply.github.com>
2021-09-11 21:45:29 -0700

    210912

---
+++ 1week/Section2_5.playground/Contents.swift
@@ -0,0 +1,25 @@
+//This is a comment
+
+var account = 100; //변수명과 = 기호 사이에 whiteSpace 있어야 함.
+
+//Comment 의 중요성: 왜 30을 빼고 5를 뺐는지의 목적을 알 수 있음.
+
+// bought fuel for car
+account = account - 30; //문장과 같은 라인에 주석을 붙여도 됨.
+
+// got some snacks
+account = account - 5;
+
+// got toothpaste
+account = account - 5;
+
+/*
+ 문단용 주석
+ */
+
+//주석을 이용해서 테스트를 하거나 임시로 코드를 치워둘 수 있음.
+
+print(45); //콘솔 출력 메소드
+print("Hello World");
+print(account);
+
