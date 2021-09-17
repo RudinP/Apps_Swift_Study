cdea8c58583a6ddc06b89936f99b59270d2b0e98
RudinP <65944851+RudinP@users.noreply.github.com>
2021-09-17 19:28:39 +0900

    210917

---
+++ 2week/MyApp/MyApp/ViewController.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+class ViewController: UIViewController {
+    //ctrl키 누르고 storyboard에서 드래그 하면 연결 가능
+    //여기서 직접 연결된 변수 이름을 바꾸면안됨. 스토리보드와 연결되지 않기 때문.
+    //스토리보드에서 연결을 끊고 다시 동일하게 불러와야함.
+    @IBOutlet weak var myLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    //동일하게 ctrl키 누르고 storyboard에서 드래그 할 때 action으로 고르면 가능
+    @IBAction func onButtonClick(_ sender: Any) {
+        view.backgroundColor = .green; //배경의 색을 초록색으로 바꾼다.
+        myLabel.text = "To be or not to be";//텍스트의 내용을 바꾼다.
+        myLabel.textColor = .white;//텍스트의 색을 초록색으로 바꾼다.
+    }
+
+}
+
---
+++ 2week/Section3_9.playground/Contents.swift
@@ -0,0 +1,20 @@
+//If statements
+
+var age = 16;
+
+if age >= 18{//<,>,<=,>=,==,!=
+    print("You are able to get driver license");
+}else{ //swift는 else 블럭을 쓰려면 if문의 블럭이 끝나는 괄호 바로 뒤에 else를 써야함
+    print("You are too young to get driver license");
+}
+
+var sound = 70;
+
+//if sound is higher than 85 print it is too loud
+//else it is quite safe
+
+if sound>=85{
+    print("It is too loud");
+}else {
+    print("It is quite safe");
+}
