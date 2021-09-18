a87da93d4aaf1ac09da608c7c2a464d57aed0cc6
RudinP <65944851+RudinP@users.noreply.github.com>
2021-09-18 17:56:44 +0900

    210918

--- 2week/MyApp/MyApp/ViewController.swift
+++ 2week/MyApp/MyApp/ViewController.swift
@@ -6,6 +6,7 @@ class ViewController: UIViewController {
     //스토리보드에서 연결을 끊고 다시 동일하게 불러와야함.
     @IBOutlet weak var myLabel: UILabel!
     
+    var countDown = 100;
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -13,9 +14,18 @@ class ViewController: UIViewController {
 
     //동일하게 ctrl키 누르고 storyboard에서 드래그 할 때 action으로 고르면 가능
     @IBAction func onButtonClick(_ sender: Any) {
-        view.backgroundColor = .green; //배경의 색을 초록색으로 바꾼다.
+        /*view.backgroundColor = .green; //배경의 색을 초록색으로 바꾼다.
         myLabel.text = "To be or not to be";//텍스트의 내용을 바꾼다.
-        myLabel.textColor = .white;//텍스트의 색을 초록색으로 바꾼다.
+        myLabel.textColor = .white;//텍스트의 색을 초록색으로 바꾼다.*/
+        countDown -= 5;
+        myLabel.text = String(countDown);
+        
+        if countDown <= 10
+        {
+            myLabel.textColor = .red; //Swift는 = 양 옆으로 스페이스가 있어야 함
+        } else if countDown <= 50{
+            myLabel.textColor = .green;
+        }
     }
 
 }
---
+++ 2week/Section4_1.playground/Contents.swift
@@ -0,0 +1,25 @@
+//Math and Doubles Operations
+(4 + 6) / 3 * 99
+
+var age = 12;
+
+var ageMin = 18;
+
+if age < ageMin{
+    let difference = ageMin - age;
+    print(difference);
+}
+
+//converter
+var i = 15.0;
+
+var m = i * (381 / 1250); //i가 double이기때문에 자동형변환
+
+381 / 1250 //integer끼리 계산은 소수점 버림
+2.0 / 5.0  //double끼리 계산은 소수점 계산 됨.
+
+var transfer = 200.0;
+
+var cut = transfer * 0.02;
+
+100_000_000 //눈에 잘 보이게 하기 위해 언더바 넣어도 됨
