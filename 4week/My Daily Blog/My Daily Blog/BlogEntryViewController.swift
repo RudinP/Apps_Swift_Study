be7d5fa36d13bb1e70a67f25faf9358644283960
RudinP <65944851+RudinP@users.noreply.github.com>
2021-10-04 15:48:05 +0900

    211004-2

--- 4week/My Daily Blog/My Daily Blog/BlogEntryViewController.swift
+++ 4week/My Daily Blog/My Daily Blog/BlogEntryViewController.swift
@@ -10,16 +10,20 @@ import UIKit
 class BlogEntryViewController: UIViewController, UITextViewDelegate {
     @IBOutlet weak var datePicker: UIDatePicker!
     @IBOutlet weak var blogEntryTextView: UITextView!
-        
+    @IBOutlet weak var botConstraint: NSLayoutConstraint!
     var blogEntry: BlogEntry?
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
         if blogEntry == nil{
             if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext{
                 blogEntry = BlogEntry(context: context);
                 blogEntry?.date = datePicker.date
                 blogEntry?.content = blogEntryTextView.text
+                blogEntryTextView.becomeFirstResponder() //키보드 팝업
             }
 
         }
@@ -33,6 +37,14 @@ class BlogEntryViewController: UIViewController, UITextViewDelegate {
         
         (UIApplication.shared.delegate as? AppDelegate)?.saveContext();
         
+    }
+    @objc func keyboardWillShow(_ notification: Notification) {
+        if let keyboardFrame: NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+            let keyboardRectangle = keyboardFrame.cgRectValue
+            let keyboardHeight = keyboardRectangle.height
+            
+            botConstraint.constant = keyboardHeight
+        }
     }
     @IBAction func onDelete(_ sender: Any) {
         if blogEntry != nil{
