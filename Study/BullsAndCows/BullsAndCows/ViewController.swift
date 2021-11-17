38591a01ca668d5d0c322fc779f013fcba7b14e9
RudinP <65944851+RudinP@users.noreply.github.com>
2021-11-17 14:30:08 +0900

    Add gameFunc

--- Study/BullsAndCows/BullsAndCows/ViewController.swift
+++ Study/BullsAndCows/BullsAndCows/ViewController.swift
@@ -9,14 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var answer: UILabel!
-    
+    @IBOutlet weak var answerWord: UITextField!
+    @IBOutlet weak var bullCow: UILabel!
+    @IBOutlet weak var button: UIButton!
+    var guess:String = ""
+    var answerCheck:String = ""
     @IBAction func AnswerChecker(_ sender: Any) {
-        answer.text = RandomWord()
+        guess = answerWord.text!
+        answerCheck = answer.text!
+        if guess.count > answerCheck.count{
+            bullCow.text = "Answer is shorter than your guess."
+        }else{
+            var count = Count()
+            count = GameFunc(guess: guess, answer: answerCheck)
+            if count.bulls == answer.text?.count{
+                bullCow.text = "You Won!"
+                self.button.setTitle("Replay", for: .normal)
+                if button.titleLabel!.text == "Replay"{
+                    self.viewDidLoad()
+                }
+            }else{
+                bullCow.text = "\(count.bulls) Bulls and \(count.cows) Cows."
+            }
+        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        bullCow.text = ""
+        self.button.setTitle("Check", for: .normal)
         answer.text = RandomWord()
+        answerWord.text = ""
         // Do any additional setup after loading the view.
     }
 
--- Study/BullsAndCows/BullsAndCows/WordChooser.swift
+++ Study/BullsAndCows/BullsAndCows/WordChooser.swift
@@ -1,6 +1,30 @@
 import Foundation
+struct Count{
+    var bulls = 0
+    var cows = 0
+}
 func RandomWord()->String{
     let index = Int.random(in: 0...wordList.count)
     let word = wordList[index]
     return word
+}
+func GameFunc(guess: String, answer: String)->Count{
+    var count = Count()
+
+    for guessIndex in 0...guess.count-1{
+        if guess[guess.index(guess.startIndex, offsetBy: guessIndex)] == answer[answer.index(answer.startIndex, offsetBy: guessIndex)]{
+            count.bulls += 1
+            continue
+        }
+        for hiddenIndex in 0...answer.count-1{
+            if guess[guess.index(guess.startIndex, offsetBy: guessIndex)] == answer[answer.index(answer.startIndex, offsetBy: hiddenIndex)]{
+                count.cows += 1
+                break;
+                    
+            }
+                
+        }
+    }
+    
+    return count
 }
